10a43ff0fa9dfd5013ad9f9c64e2bccbe34f0ce0
juampimf <juampi_mf_@hotmail.com>
2022-12-12 13:14:23 -0300

    primera subida...

---
+++ Ejemplo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
